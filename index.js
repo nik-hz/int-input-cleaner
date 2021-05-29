3853efa4a2466cd1e9b4bbab53c35ee17cb4a5c8
nik <nik0001@protonmail.com>
2021-05-29 20:36:37 +0200

    module takes str numbers, checks for NaN, formats and returns ints

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
-module.stringToIntAndClean = (number) => {
+module.intCleaner = (value) => {
     // check if for , in the str and convert to .
 
     // check for any NaN
 
     // return float
 
-    const decimal = /,/
     const numbers = /[0-9]/
 
     if (!numbers.test(value)) {
         throw new TypeError('enter a valid number', 'intCleaner')
     }
-    console.log(value)
 
     const decimalConvertedValue = value.replace(',', '.')
-
-    console.log('dec', decimalConvertedValue)
 
     const floatDecimalValue = parseFloat(decimalConvertedValue)
 
